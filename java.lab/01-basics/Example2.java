9c7aeecd895d2ad9feb791a0e8f48214040df0d3
Nurboto Mustapakulov <mustapakulov11@gmail.com>
2026-06-10 00:04:05 +0600

    using variables

---
+++ java.lab/01-basics/Example2.java
@@ -0,0 +1,15 @@
+class Example2 {
+  public static void main(String[] args) {
+    int myVar1;
+    int myVar2;
+
+    myVar1 = 1024;
+
+    System.out.println("myVar1 содержит: " + myVar1);
+
+    myVar2 = myVar1 / 2;
+
+    System.out.print("myvar2 содержит myVar1 / 2: ");
+    System.out.println(myVar2);
+  }
+}
---
+++ java.lab/01-basics/Example3.java
@@ -0,0 +1,18 @@
+class Example3 {
+  public static void main(String[] args){
+    int v;
+    double x;
+	
+    v = 10;
+    x = 10.0;
+
+    System.out.println("v = " + v);
+    System.out.println("x = " + x);
+
+    v = v / 4;
+    x = x / 4;
+    System.out.println("После деления\n");
+    System.out.println("v = " + v);
+    System.out.println("x = " + x);
+  }
+}
