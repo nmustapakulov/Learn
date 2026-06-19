32f7df8de5daa8d5969a44878532679526694746
Nurboto Mustapakulov <mustapakulov11@gmail.com>
2026-06-20 01:25:39 +0600

    Добавлено упражнение космический конвертер

---
+++ dart.lab/01-basics/space_converter.dart
@@ -0,0 +1,11 @@
+/*
+ *  Космический конвертер веса.
+*/
+void main() {
+  
+  double earthWeight = 57;
+  double lunarWeight = earthWeight * 0.165;;
+  
+  
+  print('Земной вес человека $earthWeight кг на луне будет $lunarWeight кг.');
+}
